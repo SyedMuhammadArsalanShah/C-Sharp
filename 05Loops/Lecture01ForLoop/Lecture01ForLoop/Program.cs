0ba5862944bf47f9dd4cf373224fb2d02cda2ef1
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2022-02-07 20:00:49 +0500

    update

--- 05Loops/Lecture01ForLoop/Lecture01ForLoop/Program.cs
+++ 05Loops/Lecture01ForLoop/Lecture01ForLoop/Program.cs
@@ -9,36 +9,57 @@ namespace Lecture01ForLoop
     class Program
     {
         static void Main(string[] args)
-        {            //for loop
-            //for(initialization ;condition; increment/decrement){}
-            int i, j;
-            //0to10 series
-            for (i = 0; i <= 10; i++)
+{            //for loop
+       //     //for(initialization ;condition; increment/decrement){}
+       //     int i, j;
+       //     //0to10 series
+       //     for (i = 0; i <= 10; i++)
+       //     {
+
+
+       //         Console.WriteLine("laiba" + i);
+
+       //     }
+
+       //     //0 to 100 print series with  even odd number
+       //     for (j = 0; j <= 100; j++)
+       //     {
+
+       //         if (j % 2 == 0)
+       //         {
+       //             Console.WriteLine("even" + j);
+       //         }
+         
+       //else
+       //         {
+       //             Console.WriteLine("odd" + j);
+
+       //         }
+
+
+
+       //     }
+
+            // factorial loop 
+            int fact = 1;
+            // 5*4*3*2*1=120
+            for (int i= 1; i <=5; i++)
             {
-
-
-                Console.WriteLine("laiba" + i);
-
+                fact*= i;  //fact=fact*i  =ist iteration= 1 ;2nd iteration 2;3rd= 6 4th= 24 ;5th 120 
             }
+            Console.WriteLine(fact);
 
-            //0 to 100 print series with  even odd number
-            for (j = 0; j <= 100; j++)
+            int fact1 = 0;
+            //sum of ten numbers
+            //1+2+3+4+5+6+7+8+9+10
+            int value1 = Convert.ToInt32(Console.ReadLine());
+            int value2 = Convert.ToInt32(Console.ReadLine());
+            for (int i = value1; i <= value2; i++)
             {
-
-                if (j % 2 == 0)
-                {
-                    Console.WriteLine("even" + j);
-                }
-                else
-                {
-                    Console.WriteLine("odd" + j);
-
-                }
-
-
-
+                fact1 += i; //fact1 
             }
-
+            Console.WriteLine(fact1);  //fact=fact+i = ist iteration = 1; 2nd iteration 3; 3rd = 6 4th = 10; 5th 15
+            //
 
 
             Console.ReadKey();
