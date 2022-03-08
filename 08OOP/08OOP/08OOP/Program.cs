c496c5885541643f523f8b7cc6a8cc5a79bcf13a
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2022-03-08 10:38:19 +0500

    code updated

--- 08OOP/08OOP/08OOP/Program.cs
+++ 08OOP/08OOP/08OOP/Program.cs
@@ -21,17 +21,26 @@ namespace _08OOP
             //Console.WriteLine("bname "+obj1.bname);
             //Console.WriteLine("model "+obj1.model);
 
+            Mobile mobile = new Mobile();
+            // methodone() Mobile Ki class Ka wo method hai jo without parameter Hai
+            mobile.methodone();
+            // "methodtwo()" Mobile Ki class Ka wo method hai jo with parameter Hai
+            // ab hum isko arguments dengy parameters ko value dena arguments kehlata hai 
+            mobile.methodtwo("huawei", "nova3i", 220000);
+
+
+
             Mobile obj2 = new Mobile();
-            obj2.methodone("huawei","nova3i",220000);
+            obj2.methodtwo("huawei","nova3i",220000);
 
             Mobile obj3 = new Mobile();
-            obj3.methodone("huawei1", "nova3i", 220000);
+            obj3.methodtwo("huawei1", "nova3i", 220000);
 
             Mobile obj4 = new Mobile();
-            obj4.methodone("huawei2", "nova3i", 220000);
+            obj4.methodtwo("huawei2", "nova3i", 220000);
             Mobile obj5 = new Mobile();
-            obj5.methodone("huawei3", "nova3i", 220000);
-            //Console.WriteLine(a+b+c );
+            obj5.methodtwo("huawei3", "nova3i", 220000);
+           
 
             Mobile obj6 = new Mobile();
             int uservalue = Convert.ToInt32(Console.ReadLine());
